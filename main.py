d09cc0689625d1495f2bd79d68cdbe79a23681ed
rihusedesign <97709184+rihusedesign@users.noreply.github.com>
2023-03-23 00:27:56 -0700

    final4

--- main.py
+++ main.py
@@ -51,11 +51,35 @@ def try_model(h1,h2,h3,learning_rate, l1_factor):
             outputs = model(inputs.float())
             mse_loss = criterion_mse(outputs, labels.float().unsqueeze(1))
             l1_loss = criterion_l1(torch.cat([p.view(-1) for p in model.parameters()], dim=0), torch.zeros_like(torch.cat([p.view(-1) for p in model.parameters()], dim=0)))
+<<<<<<< HEAD
+            # First grid search options -- yielded 256,128,64,0.01,1 -- lowest loss: 0.354
+# h1s = [128, 256]
+# h2s = [64, 128]
+# h3s = [32, 64]
+# lrs = [0.01, 0.0075, 0.005]
+# lambdas = [0.01,0.1,1,5]
+=======
             total_loss = mse_loss + l1_factor * l1_loss
             total_loss.backward()
             optimizer.step()
             running_train_loss += mse_loss.item()
+>>>>>>> 8bb9fd5ae10e398d28ac5647c08970c049636f6c
 
+<<<<<<< HEAD
+# Second grid search options -- yielded 256,128,64,0.01,1 again -- lowest loss: 0.353
+# h1s = [256]
+# h2s = [128]
+# h3s = [64]
+# lrs = [0.015,0.01,0.0085]
+# lambdas = [1]
+
+# Third grid search options -- yielded 256,180,64,0.01,1  -- lowest loss: 0.351
+# h1s = [256]
+# h2s = [128,180]
+# h3s = [64]
+# lrs = [0.01]
+# lambdas = [1]
+=======
         model.eval()
         running_val_loss = 0.0
         test_batch_size = min(len(test_data_x), BATCH_SIZE)
@@ -70,14 +94,35 @@ def try_model(h1,h2,h3,learning_rate, l1_factor):
                 
                 outputs = model(inputs.float())
                 running_val_loss += criterion_mse(outputs, labels.float().unsqueeze(1)).item()
+>>>>>>> 8bb9fd5ae10e398d28ac5647c08970c049636f6c
 
+<<<<<<< HEAD
+# Fourth grid search options -- yielded 320,230,64,0.01,2  -- lowest loss: 0.348
+h1s = [256,280,320,400]
+h2s = [200,230,260]
+h3s = [64]
+lrs = [0.01]
+lambdas = [1,2]
+=======
                 # When finding the distribution of predictions for a real-world scenario (integer values)
                 # Round the outputs to the nearest integer
                 rounded_outputs = torch.round(outputs)
                 errors = torch.abs(rounded_outputs - labels.float().unsqueeze(1))
                 for error in error_counts:
                     error_counts[error] += torch.sum(errors == error).item()
+>>>>>>> 8bb9fd5ae10e398d28ac5647c08970c049636f6c
 
+<<<<<<< HEAD
+def do_grid_search():
+    lowest_validation_loss = 1000.0
+    best_h1, best_h2, best_h3, best_lr, best_l1 = 0, 0, 0, 0, 0
+    for h1 in h1s:
+        for h2 in h2s:
+            for h3 in h3s:
+                for lr in lrs:
+                    for l1_fac in lambdas:
+                        val_loss = try_model(h1,h2,h3,lr,l1_fac)
+=======
                 total_samples += errors.shape[0]
         end_time = time.perf_counter()
         elapsed_time += (end_time - start_time)
@@ -140,3 +185,4 @@ def do_grid_search():
 
     print(best_h1, best_h2, best_h3, best_lr, best_l1)
     print(lowest_validation_loss)
+>>>>>>> 8bb9fd5ae10e398d28ac5647c08970c049636f6c
